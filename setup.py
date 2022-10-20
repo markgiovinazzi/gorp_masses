3d4e57e8ea66e9c230437ed68b34d09e807912b5
Mark Giovinazzi <mrg96@drexel.edu>
2022-10-20 14:43:35 -0400

    Add files via upload
    fixed broken README file. updated setup.py to account for new resources directory.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(name='gorp',
       version='1.0',
       py_modules=['gorp'],
-      packages=find_packages() + ['err_file'],
+      packages=find_packages() + ['resources'],
       include_package_data=True
       )
